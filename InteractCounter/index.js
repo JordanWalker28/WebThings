82481569eb39ef93abb0f5a7c99fbf94e1963768
Jordan Walker <jordanswalker28@gmail.com>
2021-07-27 00:10:31 +0100

    added more styling

--- InteractCounter/index.js
+++ InteractCounter/index.js
@@ -4,3 +4,27 @@ const Value = document.querySelector(".Value");
 const btns = document.querySelectorAll(".btn");
 
 console.log(btns)
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+        const styles = event.currentTarget.classList
+
+        if(styles.contains("btn-danger")){
+            num--;
+        }else if(styles.contains("btn-success")){
+            num++;
+        }else{
+            num = 0;
+        }
+
+        Value.textContent = num;
+
+        if(num > 0){
+            Value.style.color = "green"
+        }else if(num < 0){
+            Value.style.color = "red"
+        }else{
+            Value.style.color = "black"
+        }
+    })
+})
